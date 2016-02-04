08326a91f5408cc5a2a9b77bd78c818cd6dbae96
zhangyf1221 <zhangyf1221@gmail.com>
2016-02-04 14:36:38 -0500

    Initial Commit

---
+++ Landing PageTests/Landing_PageTests.swift
@@ -0,0 +1,36 @@
+//
+//  Landing_PageTests.swift
+//  Landing PageTests
+//
+//  Created by xian he on 2/4/16.
+//  Copyright © 2016 com.foodeasygo.dev. All rights reserved.
+//
+
+import XCTest
+@testable import Landing_Page
+
+class Landing_PageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
